513d8c6792e62b9d4e1693cb7f37f19fc18f9c9e
TomRiddle67 <courage0047@gmail.com>
2026-06-04 16:41:31 +0100

    feat: add get all doctors with user data populated

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createProfile } = require('../controllers/doctorController');
+const { createProfile, getAllDoctors } = require('../controllers/doctorController');
 const { protect, authorizeRoles } = require('../middleware/authMiddleware');
 
+router.get('/', getAllDoctors);
 router.post('/profile', protect, authorizeRoles('doctor'), createProfile);
 
 module.exports = router;
